f80a3484e3b4447f462bb81cb670d05e7a675cf0
CristinaDAmico <crydamico94@gmail.com>
2021-06-10 23:23:34 +0200

    commit

--- database/migrations/2021_06_10_211307_create_packages_table.php
+++ database/migrations/2021_06_10_211307_create_packages_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateTravelsTable extends Migration
+class CreatePackagesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateTravelsTable extends Migration
      */
     public function up()
     {
-        Schema::create('travels', function (Blueprint $table) {
+        Schema::create('packages', function (Blueprint $table) {
             $table->id();
             $table->string('città', 50);
             $table->string('trasporto', 25);
@@ -32,6 +32,6 @@ class CreateTravelsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('travels');
+        Schema::dropIfExists('packages');
     }
 }
